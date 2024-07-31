89c1269d03495b06adef490c3220569aff16ceb9
ls-bob <133640338+ls-bob@users.noreply.github.com>
2024-07-31 15:41:47 +0800

    Release/2.10.0 (#227)
    * feat:cancel all offers and cancel all listings (#199)
    
    * chore: add issue template and update the configuration of gitflow
    
    * feat: add royalty for sell modal (#226)

--- src/pagesComponents/Detail/component/BuyNowModal/components/TotalPrice/index.tsx
+++ src/pagesComponents/Detail/component/BuyNowModal/components/TotalPrice/index.tsx
@@ -1,12 +1,24 @@
 import ElfLogo from 'assets/images/explore/aelf.svg';
+import BigNumber from 'bignumber.js';
+import { ITransitionFee } from 'components/Summary/useGetTransitionFee';
 import { formatTokenPrice, formatUSDPrice } from 'utils/format';
 
 export default function TotalPrice(info: {
   totalPrice: string | number;
-  convertTotalPrice: string | number;
+  convertTotalPrice?: string | number;
   title?: string;
+  fee?: ITransitionFee;
+  rate: number;
 }) {
-  const { totalPrice, convertTotalPrice, title } = info;
+  const { totalPrice, title, fee = {}, rate = 1 } = info;
+  const { forestServiceRate = 0, creatorLoyaltyRate = 0 } = fee;
+  const totalPriceBig = new BigNumber(totalPrice);
+
+  const resultPrice = totalPriceBig.isNaN()
+    ? '--'
+    : totalPriceBig.times(1 - (forestServiceRate + creatorLoyaltyRate)).toNumber();
+  const priceUsd = totalPriceBig.multipliedBy(rate);
+
   return (
     <>
       <div className="flex justify-between ">
@@ -14,10 +26,10 @@ export default function TotalPrice(info: {
         <div className="flex flex-col items-end">
           <span className="flex text-[20px] leading-[28px] font-semibold text-[var(--text-primary)]">
             <ElfLogo className="mr-[8px]" />
-            {formatTokenPrice(totalPrice)} ELF
+            {formatTokenPrice(resultPrice)} ELF
           </span>
           <span className="mt-[8px] text-[16px] leading-[24px] font-medium text-[var(--text-secondary)]">
-            {formatUSDPrice(convertTotalPrice)}
+            {formatUSDPrice(priceUsd)}
           </span>
         </div>
       </div>
