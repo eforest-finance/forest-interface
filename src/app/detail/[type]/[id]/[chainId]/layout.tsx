3f1ea88509ebb45c4bef552cda12884bbb5e82ca
bob <bob@eforest.finance>
2024-09-09 15:00:51 +0800

    feat: upgrade modal styles and refactory editModal

--- src/app/detail/[type]/[id]/[chainId]/layout.tsx
+++ src/app/detail/[type]/[id]/[chainId]/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'title: test share nft to X',
+    description: 'description: test share nft to X',
+    images: [
+      {
+        url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/1725863931780-QmUagFPoGyNvAJMy7ditDuX7hbqYmJfCmhXzHEjrGEiKku.png',
+      },
+    ],
+  },
+};
+
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div className="min-h-screen">{children}</div>;
 }
