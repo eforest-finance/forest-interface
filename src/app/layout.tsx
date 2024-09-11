2c158e43e994dba807bf8623505bb2697cf9b237
bob <bob@eforest.finance>
2024-09-11 12:51:38 +0800

    feat: upgrade modal styles and refactory editModal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,16 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Forest',
   description: 'Forest',
+  twitter: {
+    title: 'title: test share nft to X',
+    description: 'description: test share nft to X',
+    images: {
+      width: 1200,
+      height: 675,
+      url: 'https://looksrare.mo.cloudinary.net/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D/0x7fa4427abb4304f7797a5ac2e95ff82965a61af6bfd81a9363f2acfaa23fb1ca?resource_type=image&f=auto&c=limit&w=1200&q=auto',
+      // url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/1725863931780-QmUagFPoGyNvAJMy7ditDuX7hbqYmJfCmhXzHEjrGEiKku.png',
+    },
+  },
   openGraph: {
     title: 'title: test share nft to X',
     description: 'description: test share nft to X',
