0c9be129de8dce73d7de9dc1d39ba28677b9b440
bob <bob@eforest.finance>
2024-09-11 13:32:37 +0800

    feat: upgrade modal styles and refactory editModal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     images: {
       width: 1200,
       height: 675,
-      url: 'https://looksrare.mo.cloudinary.net/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D/0x7fa4427abb4304f7797a5ac2e95ff82965a61af6bfd81a9363f2acfaa23fb1ca?resource_type=image&f=auto&c=limit&w=1200&q=auto',
+      url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/Banner+1.png',
       // url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/1725863931780-QmUagFPoGyNvAJMy7ditDuX7hbqYmJfCmhXzHEjrGEiKku.png',
     },
   },
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     images: {
       width: 1200,
       height: 675,
-      url: 'https://looksrare.mo.cloudinary.net/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D/0x7fa4427abb4304f7797a5ac2e95ff82965a61af6bfd81a9363f2acfaa23fb1ca?resource_type=image&f=auto&c=limit&w=1200&q=auto',
+      url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/Banner+1.png',
       // url: 'https://forest-testnet.s3.ap-northeast-1.amazonaws.com/1725863931780-QmUagFPoGyNvAJMy7ditDuX7hbqYmJfCmhXzHEjrGEiKku.png',
     },
   },
