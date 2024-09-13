72e4ee7809c55692022a20292daa71cefbfdd2cc
ls-bob <133640338+ls-bob@users.noreply.github.com>
2024-09-13 17:45:43 +0800

    Fix/v2.17.0 document (#328)
    * fix: 2049aiNFT documents

--- src/pagesComponents/FreeMint/index.tsx
+++ src/pagesComponents/FreeMint/index.tsx
@@ -56,7 +56,7 @@ export default function Detail() {
   if (!isSmallScreen) {
     return (
       <div className="min-h-[1024px] flex w-full h-full flex-col items-center justify-center">
-        <h1 className="text-[40px] font-semibold text-textPrimary">Please access using a mobile device</h1>
+        <h1 className="text-[40px] font-semibold text-textPrimary">Please access via a mobile device</h1>
         <div className="mt-[16px] text-textSecondary text-[16px]">Please try again later.</div>
         <Button
           className="mt-[48px] w-[144px] h-[48px] text-[16px] font-medium"
